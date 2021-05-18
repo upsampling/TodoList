5668d4dee49c0cdb2730c61fb62f47dbb4b9c60c
ErasmoD <daltonico1111@gmail.com>
2021-05-18 10:37:55 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import { Todo, TodoList} from './classes'
+import { crearTodoHtml } from './js/componentes'
+
+import './styles.css';
+
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+  
